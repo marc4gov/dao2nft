d514442b7c38f8cac142da707c23d4513ac5bc2c
Marc Minnee <marc4gov@users.noreply.github.com>
2022-01-13 14:42:29 +0100

    plot update

--- model/plot_utils.py
+++ model/plot_utils.py
@@ -7,7 +7,7 @@ def monte_carlo_plot(dfs, column):
   fig, ax = plt.subplots()
   x = pd.Series(dfs[0]["timestep"]).values
   for df in dfs:
-    ax.plot(x, pd.Series(df[column]).values, label=df['run'][0]) 
+    ax.plot(x, pd.Series(df[column]).values, label='Run '+ str(df['run'][0])) 
   plt.xlabel('Timestep')
   plt.ylabel(column)
   ax.legend()

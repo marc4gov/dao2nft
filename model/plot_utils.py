cd7e36c5d4dd9cf8e4a83fddfad58f535665f6e7
Marc Minnee <marc4gov@users.noreply.github.com>
2022-02-17 21:10:29 +0100

    refactoring policies

--- model/plot_utils.py
+++ model/plot_utils.py
@@ -3,14 +3,64 @@ import numpy as np
 import pandas as pd
 
 
-def monte_carlo_plot(dfs, column, column2):
+def monte_carlo_plot(dfs, column):
   fig, ax = plt.subplots()
   x = pd.Series(dfs[0]["timestep"]).values
   for df in dfs:
     ax.plot(x, pd.Series(df[column]).values, label='Run '+ str(df['run'][0])) 
-    ax.plot(x, pd.Series(df[column2]).values, label='Performer '+ str(df['substep'][0])) 
   plt.xlabel('Timestep')
   plt.ylabel(column)
   ax.legend()
   plt.title(' ' + column)
+  plt.show()
+
+
+def agent_plot(experiment):
+    """
+For any agent 
+    """
+    df = experiment
+    projects_df = df.projects
+    ldf = pd.concat([projects_df,df.timestep], axis=1)
+    pdf = ldf.projects.apply(pd.Series)
+
+    for column in pdf:
+        df2 = pd.DataFrame([vars(f) for f in pdf[agent]])
+        df3 = pd.DataFrame([vars(f) for f in df2._wallet])
+    edf = pd.concat([df3._USD, df3._ETH, df.timestep], axis=1, keys = ['USD', 'ETH', 'Timestep'])
+        
+
+    plt.figure(figsize=(20,6))
+    plt.subplot(141)
+    plt.plot(edf["Timestep"],edf["USD"],label=agent)
+    plt.xlabel('Timestep')
+    plt.ylabel('Reserves')
+    plt.legend()
+    plt.title('Asset USD')
+
+    plt.subplot(142)
+    plt.plot(edf["Timestep"],edf["ETH"],label=agent)
+    plt.xlabel('Timestep')
+    plt.ylabel('Reserves')
+    plt.legend()
+    plt.title('Asset ETH')
+       
+    plt.show()
+
+
+def monte_carlo_plot(dfs, pool=True, pool_agent='White Pool', agent='Trader', asset="USD"):
+  fig, ax = plt.subplots()
+  edfs = []
+  for df in dfs:
+    edf = make_df(df, pool=pool, pool_agent=pool_agent, agent=agent)
+    edfs.append(edf)
+  x = pd.Series(edf["Timestep"]).values
+  for ed in edfs:
+    ax.plot(x, pd.Series(ed[asset]).values) 
+  plt.xlabel('Timestep')
+  plt.ylabel(asset)
+  if pool:
+    plt.title('Reserve ' + asset + ' for ' + pool_agent)
+  else:
+    plt.title('Volume ' + asset + ' for ' + agent)
   plt.show()

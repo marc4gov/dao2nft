dc37c266e8bb9c827d0bc517f7af3e9115f5d036
Marc Minnee <marc4gov@users.noreply.github.com>
2022-02-24 15:58:40 +0100

    update notebook

--- model/plot_utils.py
+++ model/plot_utils.py
@@ -66,67 +66,18 @@ def project_plot(df, rounds, subsets=1):
         dfps[i][j].plot.line(ax = axes[:,i], subplots=True, legend=True)
 
 
-
-
-
-def monte_carlo_plot(dfs, column):
-  fig, ax = plt.subplots()
-  x = pd.Series(dfs[0]["timestep"]).values
-  for df in dfs:
-    ax.plot(x, pd.Series(df[column]).values, label='Run '+ str(df['run'][0])) 
-  plt.xlabel('Timestep')
-  plt.ylabel(column)
-  ax.legend()
-  plt.title(' ' + column)
-  plt.show()
-
-
-def agent_plot(experiment):
-    """
-For any agent 
-    """
-    df = experiment
-    projects_df = df.projects
-    ldf = pd.concat([projects_df,df.timestep], axis=1)
-    pdf = ldf.projects.apply(pd.Series)
-
-    for column in pdf:
-        df2 = pd.DataFrame([vars(f) for f in pdf[agent]])
-        df3 = pd.DataFrame([vars(f) for f in df2._wallet])
-    edf = pd.concat([df3._USD, df3._ETH, df.timestep], axis=1, keys = ['USD', 'ETH', 'Timestep'])
-        
-
-    plt.figure(figsize=(20,6))
-    plt.subplot(141)
-    plt.plot(edf["Timestep"],edf["USD"],label=agent)
-    plt.xlabel('Timestep')
-    plt.ylabel('Reserves')
-    plt.legend()
-    plt.title('Asset USD')
-
-    plt.subplot(142)
-    plt.plot(edf["Timestep"],edf["ETH"],label=agent)
-    plt.xlabel('Timestep')
-    plt.ylabel('Reserves')
-    plt.legend()
-    plt.title('Asset ETH')
-       
-    plt.show()
-
-
-def monte_carlo_plot(dfs, pool=True, pool_agent='White Pool', agent='Trader', asset="USD"):
-  fig, ax = plt.subplots()
-  edfs = []
-  for df in dfs:
-    edf = make_df(df, pool=pool, pool_agent=pool_agent, agent=agent)
-    edfs.append(edf)
-  x = pd.Series(edf["Timestep"]).values
-  for ed in edfs:
-    ax.plot(x, pd.Series(ed[asset]).values) 
-  plt.xlabel('Timestep')
-  plt.ylabel(asset)
-  if pool:
-    plt.title('Reserve ' + asset + ' for ' + pool_agent)
-  else:
-    plt.title('Volume ' + asset + ' for ' + agent)
-  plt.show()
+def voter_dfs(df, rounds, subsets=1):
+  dfps = [[copy.deepcopy(pd.DataFrame) for x in range(subsets)] for y in range(rounds)]
+  for i in range(rounds):
+    for j in range(subsets):
+      dx = df[(df["round"] == i+1) & (df["subset"] == j)]
+      dfps[i][j] = dx['projects'].apply(pd.Series)
+      dfps[i][j].rename(columns=lambda x: x.split(" ")[1], inplace=True)
+      if i > 0:
+        dfps[i][j] = arrange_projects(dfps[i-1][j], dfps[i][j])
+      dfps[i][j].reset_index(drop=True, inplace=True)
+      s = dfps[i][j].shape
+      for k in range(s[0]):
+        for m in range(s[1]):
+          dfps[i][j].loc[k][m] = dfps[i][j].loc[k][m].current_weight
+  return dfps
